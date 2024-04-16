749ed54b883ed75588d477653730b82f232321a1
khalid-nur <khalidnur150@gmail.com>
2024-04-16 02:29:09 -0500

    added a profile drop-down menu for user account and logout functionality

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Icon from "../assets/main-logo.png";
-
+import ProfileDropdown from "./ProfileDropdown";
 const Navbar = () => {
   return (
-    <header className="max-w-full mx-auto px-4 py-2 flex justify-between items-center bg-white ">
+    <header className="max-w-full mx-auto px-4 py-2 flex justify-between items-center bg-white  border-b border-zinc-300 ">
       <div className="flex items-center justify-center max-w-sm">
         <div className=" w-32 md:w-40">
           <img className=" " src={Icon} alt="" />
@@ -16,6 +16,8 @@ const Navbar = () => {
             Gallery
           </li>
         </ul>
+
+        <ProfileDropdown />
       </nav>
     </header>
   );
---
+++ client/src/components/ProfileDropdown.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { BsImage } from "react-icons/bs";
+import { BsImages } from "react-icons/bs";
+import { MdOutlineLogout } from "react-icons/md";
+import { RiAccountCircleFill } from "react-icons/ri";
+
+const ProfileDropdown = () => {
+  const [dropdown, setDropdown] = useState<null | HTMLElement>(null);
+  const isDropdownOpen = Boolean(dropdown);
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    setDropdown(event.currentTarget);
+  };
+  const closeDropdownHandler = () => {
+    setDropdown(null);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <div className="flex gap-3 items-center">
+          <Avatar sx={{ width: 35, height: 35 }}>K</Avatar>
+
+          <button onClick={handleClick}>
+            <span
+              className="text-[#6f6d71] font-semibold"
+              onClick={handleClick}
+            >
+              Khalid
+            </span>
+          </button>
+        </div>
+      </Box>
+      <Menu
+        anchorEl={dropdown}
+        open={isDropdownOpen}
+        onClose={closeDropdownHandler}
+        onClick={closeDropdownHandler}
+        sx={{ top: 10 }}
+      >
+        {/* TODO: Refactor this so it is not repeating */}
+        <MenuItem
+          sx={{
+            display: "flex",
+            gap: "5px",
+          }}
+          onClick={closeDropdownHandler}
+        >
+          <RiAccountCircleFill size={25} />
+          My account
+        </MenuItem>
+        <div className="md:hidden">
+          <MenuItem
+            sx={{
+              display: "flex",
+              gap: "5px",
+            }}
+            onClick={closeDropdownHandler}
+          >
+            <BsImage size={20} />
+            Generate
+          </MenuItem>
+          <MenuItem
+            sx={{
+              display: "flex",
+              gap: "5px",
+            }}
+            onClick={closeDropdownHandler}
+          >
+            <BsImages size={20} />
+            Gallery
+          </MenuItem>
+        </div>
+
+        <MenuItem
+          sx={{
+            display: "flex",
+            gap: "5px",
+          }}
+          onClick={closeDropdownHandler}
+        >
+          <MdOutlineLogout size={20} />
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default ProfileDropdown;
