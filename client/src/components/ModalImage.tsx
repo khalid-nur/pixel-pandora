e173d3f5e99c434a13a3dc92b842439a80edc480
khalid-nur <khalidnur150@gmail.com>
2024-05-16 21:17:30 -0500

    implemented an image download functionality to allow users to download their images and handle errors if the image URL is not recognized

--- client/src/components/ModalImage.tsx
+++ client/src/components/ModalImage.tsx
@@ -4,6 +4,7 @@ import { Box } from "@mui/material";
 import { RiDownload2Line } from "react-icons/ri";
 import { BsEyeFill } from "react-icons/bs";
 import { IGallery } from "../models/image";
+import saveAs from "file-saver";
 
 interface ModalImageProps {
   image: IGallery | null;
@@ -13,13 +14,28 @@ const ModalImage = ({ image }: ModalImageProps) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [error, setError] = useState<string | null>(null);
 
-  console.log(image);
+  const downloadImage = () => {
+    // Get the image url
+    const imageUrl = image?.ImageUrl;
+
+    // Check If the imageUrl is valid,
+    if (imageUrl) {
+      setError(null);
+
+      // Download the image
+      saveAs(imageUrl);
+    } else {
+      // If the imageUrl is not valid, set an error message
+      setError("Failed to download image: Image imageUrl is undefined");
+    }
+  };
 
   return (
     <>
       <div
-        className="absolute bottom-3 right-6 bg-white p-2 rounded-lg"
+        className="absolute bottom-3 right-6 bg-white p-2 rounded-lg cursor-pointer"
         onClick={handleOpen}
       >
         <BsEyeFill className="fill-black" />
@@ -30,7 +46,7 @@ const ModalImage = ({ image }: ModalImageProps) => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 focus:outline-none bg-white rounded-xl shadow-xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl h-auto p-2 sm:overflow-auto ">
+        <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 focus:outline-none bg-white rounded-xl shadow-xl w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-6xl xl:max-w-6xl 2xl:max-w-2xl h-auto p-2 sm:overflow-auto ">
           <div className="flex flex-col h-full w-full  lg:flex-row">
             <div className="w-full   p-2 lg:w-1/2">
               <img
@@ -49,10 +65,16 @@ const ModalImage = ({ image }: ModalImageProps) => {
                 <p className="text-[#898989] text-sm">Resolution</p>
                 <p>{image?.imageSize}</p>
               </div>
-              <button className=" flex items-center gap-2 mt-6 bg-black text-white py-2 px-4 rounded ">
+              <button
+                className=" flex items-center gap-2 mt-6 bg-black text-white py-2 px-4 rounded "
+                onClick={downloadImage}
+              >
                 <RiDownload2Line />
                 Download
               </button>
+              {error && (
+                <p className="py-2 text-sm text-red-500 italic">{error}</p>
+              )}
             </div>
           </div>
         </Box>
