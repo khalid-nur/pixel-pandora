382dd9609df6297afe9d953674fbdc41754ab300
khalid-nur <khalidnur150@gmail.com>
2024-06-09 09:59:02 -0500

    updated api to include user authentication methods getLoggedInUser, signup, login, and logout

--- client/src/api/imageGeneratorAPI.ts
+++ client/src/api/imageGeneratorAPI.ts
@@ -1,4 +1,5 @@
 import { IGallery, IGalleryInput } from "../models/image";
+import { LoginCredentials, SignUpCredentials, User } from "../models/user";
 
 // Function to fetch data
 const fetchData = async (input: RequestInfo, init?: RequestInit) => {
@@ -9,9 +10,43 @@ const fetchData = async (input: RequestInfo, init?: RequestInit) => {
   } else {
     const errorBody = await respond.json();
 
-    const errorMessage = errorBody.error;
+    const errorMessage = errorBody;
+
     throw Error(errorMessage);
   }
+};
+// Fetches the currently logged-in user
+export const getLoggedInUser = async (): Promise<User> => {
+  const response = await fetchData("/api/v1/users", { method: "GET" });
+
+  return response.json();
+};
+
+// Signs up a new user with provided credentials
+export const signup = async (credentials: SignUpCredentials): Promise<User> => {
+  const response = await fetchData("/api/v1/users/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+
+  return response.json();
+};
+
+// Logs in a user with provided credentials
+export const Login = async (credentials: LoginCredentials): Promise<User> => {
+  const response = await fetchData("/api/v1/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+
+  return response.json();
+};
+
+// Logs out the current user
+export const logout = async (): Promise<void> => {
+  await fetchData("/api/v1/users/logout", { method: "POST" });
 };
 
 // Get images data from the backend database
---
+++ client/src/models/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  email: string;
+  password: string;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SignUpCredentials {
+  username: string;
+  email: string;
+  password: string;
+}
