1e61f050d59475342dbdfffa368ee9a164b1dc2b
khalid-nur <khalidnur150@gmail.com>
2024-06-11 23:32:08 -0500

    updated routing in app component with conditional access control based on user authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ImageGenerator from "./pages/ImageGenerator";
 import ImageGallery from "./pages/ImageGallery";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useAuth } from "./hooks/useAuthContext";
+
 const App = () => {
+  const { authReady, user } = useAuth();
+  console.log(authReady);
+
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/image-generator" element={<ImageGenerator />} />
-          <Route path="/image-gallery" element={<ImageGallery />} />
-        </Routes>
-      </Router>
+      {authReady ? (
+        <Router>
+          {user && <Navbar />}
+          <Routes>
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/image-generator" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/image-generator" />}
+            />
+            <Route
+              path="/image-generator"
+              element={user ? <ImageGenerator /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/image-gallery"
+              element={user ? <ImageGallery /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </Router>
+      ) : (
+        <div>Loading spinner or animation...</div>
+      )}
     </div>
   );
 };
